d11ddce1989e8c5c8f418aea9191e41509f20f90
dungurea <diana.ungureanu@centric.eu>
2024-10-25 08:34:14 +0300

    revert

--- frontend/src/app/components/merchant-dialog/merchant-dialog.component.ts
+++ frontend/src/app/components/merchant-dialog/merchant-dialog.component.ts
@@ -207,7 +207,7 @@ export class MerchantDialogComponent implements OnInit {
 		const translatedValue = this.translateService.instant(toasterMessage);
 
 		this.snackBar.openFromComponent(CustomSnackbarComponent, {
-			duration: 80000,
+			duration: 8000,
 			data: new SnackbarData(translatedValue, SnackbarType.SUCCESS),
 			horizontalPosition: 'right',
 			verticalPosition: 'bottom'
