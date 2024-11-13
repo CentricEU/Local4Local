bbe7957f6455954ece168274ef14e12c017de0f9
dungurea <diana.ungureanu@centric.eu>
2024-11-13 09:18:45 +0200

    fix lint

--- frontend/src/app/components/footer/footer.component.ts
+++ frontend/src/app/components/footer/footer.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
 	styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
-	public openDialog(): void {}
+	public openDialog(): void {
+		console.log('to be implemented');
+	}
 }
