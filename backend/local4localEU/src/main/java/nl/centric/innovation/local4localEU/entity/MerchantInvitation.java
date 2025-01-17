67501bdcc046e0e058d6e1b1e900072338ed84d1
dungurea <diana.ungureanu@centric.eu>
2025-01-17 11:35:55 +0200

    [DB]  New columns to the invitation table: is_registered , token, expiration time for token

--- backend/local4localEU/src/main/java/nl/centric/innovation/local4localEU/entity/MerchantInvitation.java
+++ backend/local4localEU/src/main/java/nl/centric/innovation/local4localEU/entity/MerchantInvitation.java
@@ -9,6 +9,9 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 @EqualsAndHashCode(callSuper = true)
 @Entity
 @Table(schema = "l4l_eu_global", name = "merchant_invitation")
@@ -18,12 +21,34 @@ import lombok.NoArgsConstructor;
 @Builder
 public class MerchantInvitation extends BaseEntity {
 
-	@Column(name = "email")
-	private String email;
+    @Column(name = "email")
+    private String email;
 
-	@Column(name = "message")
-	private String message;
+    @Column(name = "message")
+    private String message;
 
-	@Column(name = "is_active")
-	private boolean isActive;
+    @Column(name = "is_active")
+    private boolean isActive;
+
+    @Column(name = "is_registered")
+    private Boolean isRegistered;
+
+    @Column(name = "token")
+    private UUID token;
+
+    @Column(name = "token_expiration_date")
+    private LocalDateTime tokenExpirationDate;
+
+    public static MerchantInvitation of(String email, String message) {
+        LocalDateTime tokenExpirationDate = LocalDateTime.now().plusDays(2);
+
+        return MerchantInvitation.builder()
+                .email(email)
+                .message(message)
+                .isActive(true)
+                .isRegistered(false)
+                .token(UUID.randomUUID())
+                .tokenExpirationDate(tokenExpirationDate)
+                .build();
+    }
 }
